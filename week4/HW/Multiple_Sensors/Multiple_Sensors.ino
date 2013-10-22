761976fa0ac4120d270dd15fe02e7b9fa5c3d186
adarshk <test@172-26-12-101.dynapool.nyu.edu>
2013-10-22 11:52:38 -0400

    week 4-6 commits

--- week4/HW/Multiple_Sensors/Multiple_Sensors.ino
+++ week4/HW/Multiple_Sensors/Multiple_Sensors.ino
@@ -3,6 +3,10 @@
 int sensorValue1;
 int sensorValue2;
 int sensorValue3;
+int sensorValue4;
+int sensorValue5;
+int sensorValue6;
+int sensorValue7;
 
 boolean selected = false;
 
@@ -20,25 +24,48 @@ void loop() {
     
   //if (Serial.available() > 0) {
      // read the incoming byte:
-    int inByte =Serial.read();
+    //int inByte =Serial.read();
    // read the sensor:
-   sensorValue1 =analogRead(A0);
+  // sensorValue1 =analogRead(A0);
    // print the results:
-  Serial.print(sensorValue1, DEC);
+  //Serial.print(sensorValue1, DEC);
+  //Serial.print(",");
+
+   // read the sensor:
+   //sensorValue2 =analogRead(A1);
+   // print the results:
+  //Serial.print(sensorValue2, DEC);
+  //Serial.print(",");
+  
+     // read the sensor:
+   sensorValue3 =analogRead(A2);
+   // print the results:
+  Serial.print(sensorValue3, DEC);
+  Serial.print(",");
+  
+     // read the sensor:
+   sensorValue4 =analogRead(A3);
+   // print the results:
+  Serial.print(sensorValue4, DEC);
+  Serial.print(",");
+  
+     // read the sensor:
+   sensorValue5 =analogRead(A4);
+   // print the results:
+  Serial.print(sensorValue5, DEC);
   Serial.print(",");
 
    // read the sensor:
-   sensorValue2 =analogRead(A1);
-   // print the results:
-  Serial.print(sensorValue2, DEC);
-  Serial.print(",");
-
-   // read the sensor:
-   sensorValue3 =digitalRead(2);
+   //sensorValue6 =digitalRead(2);
    // print the last sensor value with a println() so that
    // each set of four readings prints on a line by itself:
-  Serial.println(sensorValue3, DEC);
+  //Serial.println(sensorValue6, DEC);
    //}
+   
+   sensorValue7 =analogRead(A5);
+   // print the results:
+  Serial.println(sensorValue7, DEC);
+   
   
   /*
   int sensorValue = analogRead(A0);
@@ -72,6 +99,8 @@ void loop() {
     }
     
     */
+    
+   // delay(500);
  }
  
  
